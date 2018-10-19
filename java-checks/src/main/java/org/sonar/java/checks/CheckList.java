00a5a5f4cf294c2820092974b031802c712fa60f
Christophe Zürn <christophe.zurn@sonarsource.com>
2018-10-19 16:46:49 +0200

    SONARJAVA-2906 Rule S4823: Using command line arguments is security-sensitive

--- java-checks/src/main/java/org/sonar/java/checks/CheckList.java
+++ java-checks/src/main/java/org/sonar/java/checks/CheckList.java
@@ -44,6 +44,7 @@ import org.sonar.java.checks.naming.MethodNamedEqualsCheck;
 import org.sonar.java.checks.naming.MethodNamedHashcodeOrEqualCheck;
 import org.sonar.java.checks.security.AESAlgorithmCheck;
 import org.sonar.java.checks.security.CipherBlockChainingCheck;
+import org.sonar.java.checks.security.CommandLineArgumentsCheck;
 import org.sonar.java.checks.security.CookieHttpOnlyCheck;
 import org.sonar.java.checks.security.CookieShouldNotContainSensitiveDataCheck;
 import org.sonar.java.checks.security.CryptographicKeySizeCheck;
@@ -618,6 +619,7 @@ public final class CheckList {
       .add(GetClassLoaderCheck.class)
       .add(ObjectOutputStreamCheck.class)
       .add(VolatileVariablesOperationsCheck.class)
+      .add(CommandLineArgumentsCheck.class)
       .build();
   }
 
